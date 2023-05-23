99d62532d19690bdfc3c7e89d9a1f1f479573f6a
Ilya Ershov <i.ershov@trueengineering.ru>
2023-05-23 19:29:31 +0600

    Removed unnecessary UI Code and packages

---
+++ ClientApp/src/app/modules/application/application.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EndpointsModule } from '../endpoints/endpoints.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EndpointsModule
+  ]
+})
+export class ApplicationModule { }
